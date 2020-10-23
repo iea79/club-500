12c3eb804d5dba1030ea0be99f77aa6f9922f74e
anastasia2810 <nastya-popova2810@mail.ru>
2020-10-23 14:37:09 +0500

    Start

---
+++ js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $('.header__toggle').click(function(){
+        $('.menu').addClass('open');
+        $('.header').addClass('active');
+        $(document).mousedown(function (e){
+            var menu = $('.menu'); 
+            if (!menu.is(e.target)
+            && menu.has(e.target).length === 0) {
+                $(menu).removeClass('open');
+            	$('.header').removeClass('active')
+            }
+        });
+    });
+
+    $('.menu__close').click(function(){
+        $('.menu').removeClass('open');
+        $('.header').removeClass('active');
+    });
+
+    $('.menu__link').hover(function(){
+        $('.menu__listsub').removeClass('open');
+        $('.menu__link').removeClass('active');
+        $(this).next().addClass('open');
+        $(this).addClass('active');
+    });
+
+});
