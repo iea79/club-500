62f5879b8c8f48d4eebce567746ea44b7f5ae739
Evgeniy Ivanov <busforward@gmail.com>
2020-11-14 02:31:56 +0500

    Optimize and clean

--- js/animated.js
+++ js/animated.js
@@ -1,9 +1,9 @@
+// import './custom.js';
 import LocomotiveScroll from 'locomotive-scroll';
 import { gsap, TweenLite, TweenMax, TimelineLite, TimelineMax } from "gsap";
 
 
 let container = document.querySelector('[data-scroll-container]'),
-    // elem = document.querySelector('[data-scroll]'),
     soc = document.querySelector('.major__networks'),
     aTitle = document.querySelector('.about__title'),
     year = document.querySelector('.major__year'),
@@ -25,192 +25,109 @@ let container = document.querySelector('[data-scroll-container]'),
     domSliderContent,
     domVisibled = true;
 
-const pageScroll = new LocomotiveScroll({
-    el: container,
-    smooth: true,
-    smoothMobile: false,
-    getDirection: true,
-    // scrollFromAnywhere: true
-    repeat: true
-});
+function getCallOnScroll() {
+    const pageScroll = new LocomotiveScroll({
+        el: container,
+        smooth: true,
+        smoothMobile: false,
+        getDirection: true,
+        // scrollFromAnywhere: true
+        repeat: true
+    });
 
+    pageScroll.on('scroll', (obj) => {
+        // $(document).trigger(obj);
+        // console.log(obj);
+        // console.log(obj.scroll.y);
+        // toffset = window.offset().top;
+        // console.log(obj, a, b);
+        domSliderContent = $('.dom__slider__top__convert, .dom__slider_map, .dom .slick-arrow, .dom__slider__bottom, .dom__counter'),
+        tpos = obj.scroll.y;
+        let scale,
+            opacity,
+            currentTop;
 
-pageScroll.on('scroll', (obj) => {
-    // $(document).trigger(obj);
-    // console.log(obj);
-    // console.log(obj.scroll.y);
-    // toffset = window.offset().top;
-    // console.log(obj, a, b);
-    domSliderContent = $('.dom__slider__top__convert, .dom__slider_map, .dom .slick-arrow, .dom__slider__bottom, .dom__counter'),
-    tpos = obj.scroll.y;
-    let scale,
-        opacity,
-        currentTop;
-
-    if (domScreen) {
-        // $('.domScreen').css({
-        //     top: tpos - currentTop,
-        // });
-        // $('.domScreen__block_img').css({
-        //     transform: 'scale('+scale+')'
-        // });
-
-        console.log(domScreen);
-    }
-
-    if (domScreenImg) {
-        // console.log('domScreenImg in');
-    // } else {
-        // console.log('domScreenImg out');
-    }
-
-    if (dom) {
-        // console.log(domTop);
-        // console.log(tpos);
-
-        scale = -( domTop - tpos - screenHeight )/screenHeight;
-        opacity = -scale+2;
-        // console.log(scale);
-        // console.log(opacity);
-        domSliderContent.css('opacity', '0');
-
-        if (scale >= 1) {
-            // scale = scale*1.1;
-            scaledImg = scale*(scale**scale);
-            // console.log(scale);
-            console.log(scaledImg);
-            // console.log(domVisibled);
-            // console.log(obj.direction);
+        if (dom) {
+            // console.log(domTop);
             // console.log(tpos);
-            console.log('opacity', opacity);
-            console.log('opacity *', opacity*scale*scale*scale+1.5);
-            console.log('');
-            currentTop = tpos;
-            $('.dom').css({
-                transform: 'translate(0, '+(tpos - domTop)+'px)',
-            });
-            domScreenText.css({
-                transform: 'translate(0, '+(tpos - domTop)+'px)',
-            });
-            domScreenText.css('opacity', '0');
-            domImg.css({
-                transform: 'translate(0, '+(tpos - domTop)+'px) scale('+scaledImg+')',
-                opacity: opacity*scale*scale*scale+1.5
-            });
 
-            if (scaledImg > 12) {
-                domSliderContent.css('opacity', '1');
-                slider.css('zIndex', 15);
+            scale = -( domTop - tpos - screenHeight )/screenHeight;
+            opacity = -scale+2;
+            // console.log(scale);
+            // console.log(opacity);
+            domSliderContent.css('opacity', '0');
 
+            if (scale >= 1) {
+                // scale = scale*1.1;
+                scaledImg = scale*(scale**scale);
+                // console.log(scale);
+                // console.log(scaledImg);
+                // console.log(domVisibled);
+                // console.log(obj.direction);
+                // console.log(tpos);
+                // console.log('opacity', opacity);
+                // console.log('opacity *', opacity*scale*scale*scale+1.5);
+                // console.log('');
+                currentTop = tpos;
+                $('.dom').css({
+                    transform: 'translate3d(0px, '+(tpos - domTop)+'px, 0px)',
+                });
+                domScreenText.css({
+                    transform: 'translate3d(0px, '+(tpos - domTop)+'px, 0px)',
+                });
+                domScreenText.css('opacity', '0');
+                domImg.css({
+                    transform: 'translate3d(0px, '+(tpos - domTop)+'px, 0px) scale('+scaledImg+')',
+                    opacity: opacity*scale*scale*scale+1.5
+                });
+
+                if (scaledImg > 12) {
+                    domSliderContent.css('opacity', '1');
+                    slider.css('zIndex', 15);
+
+                } else {
+                    domSliderContent.css('opacity', '0');
+                    slider.css('zIndex', 10);
+                }
             } else {
-                domSliderContent.css('opacity', '0');
-                slider.css('zIndex', 10);
+                domScreenText.css('opacity', '1');
             }
-        } else {
-            domScreenText.css('opacity', '1');
+
         }
 
+    });
+
+    pageScroll.on('call', (func, state, obj) => {
+        // $(document).trigger(obj);
+        // console.log(func);
+        switch (func) {
+            case 'dom':
+                if (state === 'enter') {
+                    console.log(obj.top);
+                    console.log('dom', state);
+                    // pageScroll.stop();
+                    // pageScroll.update();
+                    dom = true;
+                    domTop = obj.top;
+                } else {
+                    console.log('dom', state);
+                    console.log(obj.top);
+                    $('.domScreen').removeAttr('style');
+
+                    // pageScroll.start();
+                    // pageScroll.update();
+                    dom = false;
+                }
+                return ;
+            default:
+                return;
+        }
+    });
+
+    if (isXsWidth()) {
+        pageScroll.destroy();
+        $('[data-scroll]').removeAttr('style');
     }
-
-    if (domEnd) {
-    } else {
-
-    }
-});
-
-pageScroll.on('call', (func, state, obj) => {
-    // $(document).trigger(obj);
-    // console.log(func);
-    switch (func) {
-        case 'about':
-            // console.log(func, state, obj);
-            if (state === 'enter') {
-                // console.log();
-                // console.log(obj);
-                // console.log(obj.top);
-                // let p = $(obj.el).find('[data-scroll-speed]');
-            } else {
-                // pageScroll.update();
-            }
-            return ;
-        case 'domImg':
-            // console.log(func, state, obj);
-            if (state === 'enter') {
-                // console.log();
-                // console.log('domImg', state);
-                domScreenImg = true;
-                // console.log(obj.top);
-                // let p = $(obj.el).find('[data-scroll-speed]');
-            } else {
-                // console.log('domImg', state);
-                domScreenImg = false;
-                // pageScroll.update();
-            }
-            return ;
-        case 'domScreen':
-            if (state === 'enter') {
-                domScreen = false;
-                console.log('domScreen', state);
-
-                // domImg500.addClass('scaled');
-                // domScreenText.fadeOut();
-                // setTimeout(function () {
-                //     if (domImg500.hasClass('scaled')) {
-                //         domImg.fadeOut(500);
-                //         domVisibled = false;
-                //         domSliderContent.fadeIn(500);
-                //     }
-                // }, 1500);
-            } else {
-                domScreen = true;
-                console.log('domScreen', state);
-
-                // domImg500.removeClass('scaled');
-                // domScreenText.fadeIn();
-                // domVisibled = true;
-                // if (!domImg500.hasClass('scaled')) {
-                //     domImg.show();
-                // }
-            }
-            return ;
-        case 'dom':
-            if (state === 'enter') {
-                console.log(obj.top);
-                console.log('dom', state);
-                // pageScroll.stop();
-                // pageScroll.update();
-                dom = true;
-                domTop = obj.top;
-            } else {
-                console.log('dom', state);
-                console.log(obj.top);
-                $('.domScreen').removeAttr('style');
-
-                // pageScroll.start();
-                // pageScroll.update();
-                dom = false;
-            }
-            return ;
-        case 'dom-end':
-            if (state === 'enter') {
-                domEnd = true;
-            } else {
-                domScreen = false;
-                domEnd = false;
-            }
-            return;
-        default:
-        return;
-    }
-});
-if (isXsWidth()) {
-    pageScroll.destroy();
-    $('[data-scroll]').removeAttr('style');
-
-}
-
-
-(function() {
 
     $('.media__logo').click(function(){
         let id = $(this).attr('data-modal-id'),
@@ -352,7 +269,6 @@ if (isXsWidth()) {
         });
     }
 
-    // console.log($(cursor).attr('style'));
     function initCursors() {
         if (isDesktop) {
             if ($(cursor).attr('style') == undefined) {
@@ -380,6 +296,17 @@ if (isXsWidth()) {
             return false;
         }
         initCursors();
+
+        $('.menu__link, .menu__linksub').on('click', function(e) {
+            e.preventDefault();
+            let id = $(this).attr('href');
+
+            console.log(id);
+
+            $('.menu').removeClass('open');
+            $('.header').removeClass('active');
+            pageScroll.scrollTo(id);
+        })
     });
 
     $(window).resize(function() {
@@ -390,7 +317,6 @@ if (isXsWidth()) {
         } else {
             pageScroll.destroy();
             $('[data-scroll-section]').removeAttr('style');
-            return false;
         }
         initCursors();
     });
@@ -406,7 +332,6 @@ if (isXsWidth()) {
 
         $(".dom__slider__top, .dom__slider__bottom").on('init', function(event, slick, currentSlide){
             if (isDesktop) {
-                // pageScroll.update();
                 pageScroll.start();
             }
         });
@@ -471,7 +396,6 @@ if (isXsWidth()) {
         $('.header').removeClass('effect');
         $('#majorVideo').trigger('play');
         if (isDesktop) {
-            // pageScroll.update();
             pageScroll.start();
         }
     });
@@ -495,20 +419,15 @@ if (isXsWidth()) {
         $('.major__video-current').stop(true,true).animate({'width':(currentTime +.25)/duration*100+'%'},250,'linear');
     });
 
-    $('body').on('click', () => {
-        // updateScroll();
-    });
+}
+
+getCallOnScroll();
+
+let options = {
+    root: container,
+    rootMargin: '0px',
+    threshold: 1.0
+}
 
 
-    function updateScroll() {
-        console.log('updateScroll');
-        if (isDesktop) {
-            // pageScroll.update();
-        } else {
-            // pageScroll.destroy();
-        }
-
-    }
-
-
-})();
+let observer = new IntersectionObserver(getCallOnScroll, options);
